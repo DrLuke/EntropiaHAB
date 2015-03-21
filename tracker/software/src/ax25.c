6dd7466b81c7a3a296d32af5bffd98a610fd960c
Florian Larysch <fl@n621.de>
2015-03-21 02:45:59 +0100

    aprs: add software CRC implementation

--- tracker/software/src/ax25.c
+++ tracker/software/src/ax25.c
@@ -1,11 +1,27 @@
 #include "ax25.h"
 
+static uint16_t crc16(uint8_t *data, unsigned int len)
+{
+        uint16_t crc = 0xffff;
+        while (len--)
+	{
+                uint8_t c = *data++;
+
+                for (uint8_t i = 0x01; i & 0xff; i <<= 1)
+		{
+			crc ^= c & i;
+                        if(crc & 1)
+                                crc = (crc >> 1) ^ 0x8408;
+                        else
+                                crc = crc >> 1;
+                }
+        }
+
+        return (~crc) & 0xffff;
+}
+
 int ax25_constructPacket(char* packet, char* dest, char* source, char* info, int infolen)
 {
-	// Assemble AX.25 UI-Frame
-	// See http://www.aprs.org/doc/APRS101.PDF for information on how the packet is concstructed
-	crc_reset();
-
 	// Flag (1 Byte)
 	packet[0] = 0x7e;	// First Byte is always 0x7e
 
@@ -47,7 +63,7 @@ int ax25_constructPacket(char* packet, char* dest, char* source, char* info, int
 	}
 
 	// FCS (2 Bytes)
-	int crc = crc_calculate_block((uint32_t*)packet+1, infolen + 16);	// CRC between first flag and FCS field
+	int crc = crc16((uint8_t*)packet+1, infolen + 16);	// CRC between first flag and FCS field
 	packet[17+infolen+1] = (crc & 0xFF);	// LSB first
 	packet[17+infolen+2] = ((crc >> 8) & 0xFF);	// MSB last
 
